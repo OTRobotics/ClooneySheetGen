71073d217cb7ccd4f47872d823b40d32b77214b2
Jamie Sinn <james.sinn@sinndevelopment.com>
2018-09-14 23:15:40 -0400

    Create test_buildpdf.py

---
+++ test_buildpdf.py
@@ -0,0 +1,8 @@
+import json
+
+def test_sheetGeneration():
+    config = json.load(open("resources/powerup_config.json", "r", encoding="utf-8"))
+    gen = Sheet(config)
+    fields = json.load(open("resources/powerup_test_3.json", "r", encoding="utf-8"))
+    gen.create_from_json(fields)
+    pass
