0e37dbfab3fca3f6bcc5bd37726cabf6a09a96ef
Jamie Sinn <james.sinn@sinndevelopment.com>
2018-09-14 23:14:04 -0400

    Update run.py

--- run.py
+++ run.py
@@ -8,5 +8,9 @@ if __name__ == "__main__":
     fields = json.load(open("resources/powerup_test_3.json", "r", encoding="utf-8"))
     gen.create_from_json(fields)
     
-def test_placeholder():
+def test_sheetGeneration():
+    config = json.load(open("resources/powerup_config.json", "r", encoding="utf-8"))
+    gen = Sheet(config)
+    fields = json.load(open("resources/powerup_test_3.json", "r", encoding="utf-8"))
+    gen.create_from_json(fields)
     pass
